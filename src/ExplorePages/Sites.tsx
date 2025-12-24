c617eca5fb8b82099e3ba9e7788182c340df0228
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-12-24 19:48:39 +0200

    Updated p tag in Sites page

--- src/ExplorePages/Sites.tsx
+++ src/ExplorePages/Sites.tsx
@@ -6,7 +6,7 @@ export default function Sites() {
   return (
     <main style={{ padding: '2rem', color: '#ffffff', minHeight: '100vh' }}>
       <h2>Sites</h2>
-      <p>Introduce your team members here.</p>
+      <p>We will be updating these accordingly</p>
 
       {/* Return to Explore Menu */}
       <button
