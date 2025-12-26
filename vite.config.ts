f3b28f77d520dfa51d7d4c9334d11a0b971e9b22
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-12-26 11:57:19 +0200

    Updated vite config file optimized for web performance tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) return 'vendor';
+          if (id.includes('office_small')) return 'office_small';
+          if (id.includes('meeting_room')) return 'meeting_room';
+          if (id.includes('living_room')) return 'living_room';
+          if (id.includes('music_room')) return 'music_room';
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500,
+  },
+});
